870f89a0ab8db4cc3a70eb033dbbefdf365ae5d7
demo-zhang <demo-zhang@qq.com>
2025-12-11 23:58:00 +0800

    完成Q1

--- tracking/inference.py
+++ tracking/inference.py
@@ -56,12 +56,21 @@ def constructBayesNet(gameState: hunters.GameState):
     Y_RANGE = gameState.getWalls().height
     MAX_NOISE = 7
 
-    variables = []
-    edges = []
+    variables = [PAC, GHOST0, GHOST1, OBS0, OBS1]
+    edges = [(PAC, OBS0), (PAC, OBS1), (GHOST0, OBS0), (GHOST1, OBS1)]
     variableDomainsDict = {}
 
     "*** YOUR CODE HERE ***"
-    raiseNotDefined()
+    pos = []
+    for i in range(X_RANGE):
+        for j in range(Y_RANGE):
+            if not gameState.hasWall(i, j) or i == 0 or j == 0 or i == X_RANGE - 1 or j == Y_RANGE - 1:
+                pos.append((i, j))
+    variableDomainsDict[PAC] = pos.copy()
+    variableDomainsDict[GHOST0] = pos.copy()
+    variableDomainsDict[GHOST1] = pos.copy()
+    variableDomainsDict[OBS0] = [i for i in range(X_RANGE + Y_RANGE + MAX_NOISE -1)]
+    variableDomainsDict[OBS1] = [i for i in range(X_RANGE + Y_RANGE + MAX_NOISE -1)]
     "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
